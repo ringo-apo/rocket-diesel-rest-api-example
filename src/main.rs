6cb0cdfb83c561ef7df4c95a433123ae8f82bebe
ringo-apo <ringo1625@gmail.com>
2021-04-14 18:26:17 +0900

    20210414

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate diesel;
+
+use rocket_contrib::{Json, Value};
+
+mod db;
+mod schema;
+
+mod hero;
+use hero::Hero;
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>, connection: db::Connection) -> Json<Hero> {
+    let insert = Hero { id: None, ..hero.into_inner() };
+    Json(Hero::create(insert, &connection))
+}
+
+#[get("/")]
+fn read(connection: db::Connection) -> Json<Value> {
+    Json(json!(Hero::read(&connection)))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>, connection: db::Connection) -> Json<Value> {
+    let update = Hero { id: Some(id), ..hero.into_inner() };
+    Json(json!({
+        "success": Hero::update(id, update, &connection)
+    }))
+}
+
+#[delete("/<id>")]
+fn delete(id: i32, connection: db::Connection) -> Json<Value> {
+    Json(json!({
+        "success": Hero::delete(id, &connection)
+    }))
+}
+
+fn main() {
+    rocket::ignite()
+        .manage(db::connect())
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
+        .launch();
+}
